97ade3caa86a520eef610e1c5a84f5f522257971
悠哉 <2086189633@qq.com>
2022-01-18 00:14:34 +0800

    修改url

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func banner() {
 
 // 生成目标信息
 func target_Info() {
-	url := "https://212.35.103.196"
+	url := "https://xx.xx.xx.xx"
 	userAgent := "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1"
 	active.Target.Target_url = url
 	active.Target.User_agent = userAgent
